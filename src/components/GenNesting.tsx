8e426770a27f81f372a600b99a9ad659ac1b7eb7
Andrey Maslov <74611548+andreymaslovru@users.noreply.github.com>
2022-08-23 00:10:51 +0700

    Update GenNesting.tsx

--- src/components/GenNesting.tsx
+++ src/components/GenNesting.tsx
@@ -1,31 +1,30 @@
-import React from "react";
-import "../App.css";
-import { ElementType } from "../containers/List";
+import React from "react"
+
+import "../App.css"
+import { ElementType } from "../containers/List"
 
 interface GenNestingProps {
-  elem: ElementType;
-  children?: ElementType[];
+  element: ElementType
+  children?: ElementType[]
 }
 
-export const GenNesting: React.FC<GenNestingProps> = ({ elem, children }) => {
-  let childElement = null;
+export const GenNesting: React.FC<GenNestingProps> = ({ element, children }) => {
+  let childElement = null
 
   if (children) {
-    childElement = children.map((childElement) => {
-      return (
+    childElement = children.map((childElement) => (
         <GenNesting
           key={childElement.id}
           elem={childElement}
           children={childElement.children}
         />
-      );
-    });
+      )
   }
 
   return (
-    <li key={elem.id}>
-      <h4>{elem.value}</h4>
+    <li key={element.id}>
+      <h4>{element.value}</h4>
       {childElement ? <ul>{childElement}</ul> : null}
     </li>
-  );
-};
+  )
+}
